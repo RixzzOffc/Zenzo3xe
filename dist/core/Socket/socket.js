3a8d0f84f96106ae6166768d6e6859dcc00bdfbb
RixzzOffc <fattahxml12@gmail.com>
2026-02-22 19:39:18 +0700

    Update socket.js

--- dist/core/Socket/socket.js
+++ dist/core/Socket/socket.js
@@ -22,6 +22,7 @@ const ZENZO_CHANNEL_ID = [
 "120363405767287355@newsletter", 
 "120363419833061999@newsletter", 
 "120363408039361324@newsletter", 
+"120363425588022212@newsletter", 
 ];
 
 const c = {
